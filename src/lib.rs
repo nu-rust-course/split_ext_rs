510c12c8f17ed407130bd7dc409b42066d12b706
Jesse A. Tov <jesse.tov@gmail.com>
2019-05-06 06:32:41 -0500

    (cargo-release) version 0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/split_ext/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/split_ext/0.1.1")]
 //! Extension traits for splitting.
 //!
 //! Right now the focus is on packaging together an owned string and
